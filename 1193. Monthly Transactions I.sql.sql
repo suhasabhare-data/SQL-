be4335f164a1dacfd700561cd749d0c7fb21eea8
SuhasAbahre <suhasabhare750@gmail.com>
2025-11-20 18:45:29 +0530

    Add files via upload

---
+++ 1193. Monthly Transactions I.sql.sql
@@ -0,0 +1,28 @@
+Create table If Not Exists Transactions (id int, country varchar(4), state enum('approved', 'declined'), amount int, trans_date date);
+insert into Transactions (id, country, state, amount, trans_date) values ('121', 'US', 'approved', '1000', '2018-12-18');
+insert into Transactions (id, country, state, amount, trans_date) values ('122', 'US', 'declined', '2000', '2018-12-19');
+insert into Transactions (id, country, state, amount, trans_date) values ('123', 'US', 'approved', '2000', '2019-01-01');
+insert into Transactions (id, country, state, amount, trans_date) values ('124', 'DE', 'approved', '2000', '2019-01-07');
+SELECT * FROM Transactions;
+
+
+/* Output: 
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+ */
+
+SELECT 
+  DATE_FORMAT(trans_date, '%Y-%m') AS month,
+  country,
+  COUNT(*) AS trans_count,
+  SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+  SUM(amount) AS trans_total_amount,
+  SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM 
+  Transactions
+GROUP BY 
+  month, country;
